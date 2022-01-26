d864e4189e878c5efe435ecf212604508ccde361
Silvan Miller <silvanmiller@gmail.com>
2022-01-26 09:18:35 -0300

    Criação aula11

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,6 +1,6 @@
-var pais = 'EUA'
+var pais = 'Brasil'
 console.log(`Vivendo em ${pais}`)
-if (pais == 'EUA') {
+if (pais != 'Brasil') {
     console.log(`Você é um estrangeiro`)
 } else
     console.log(`Você é um brasileiro`)
