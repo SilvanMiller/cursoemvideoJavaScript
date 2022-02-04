7c8b0f51730a12ead66d8082e61305317ce5d8c4
Silvan Miller <silvanmiller@gmail.com>
2022-02-04 10:00:47 -0300

    criação aula13

--- aula13ex/modelo/script.js
+++ aula13ex/modelo/script.js
@@ -1,11 +1,20 @@
 function tabuada() {
-    let numer = document.querySelector('#txtN');
-    let tabua = document.querySelectorAll('div#seltab');
-    if (numer.value.length == 0) {
-        alert('Por favor, Digite um número!')
+    var numero = document.getElementById('txtn');
+    var tabuad = document.getElementById('seltabu');
 
+    if (numero.value.length == 0) {
+        alert('Por favor, Digite um número!');
     } else {
-        let n = Number(numer.value)
+        var n = Number(numero.value);
+        var c = 1;
+
+        while (c <= 10) {
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n*c}`;
+            tabuad.appendChild(item);
+            c++
+        }
+
     }
 
 }
