68a03070748607158f645c27bd23394a95528d7f
Silvan Miller <silvanmiller@gmail.com>
2022-02-04 08:02:32 -0300

    exercicios

---
+++ aula13ex/modelo/script.js
@@ -0,0 +1,11 @@
+function tabuada() {
+    let numer = document.querySelector('#txtN');
+    let tabua = document.querySelectorAll('div#seltab');
+    if (numer.value.length == 0) {
+        alert('Por favor, Digite um número!')
+
+    } else {
+        let n = Number(numer.value)
+    }
+
+}
