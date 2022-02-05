fb5ea23faba161f9efa5089e8cd884ed2e713270
Silvan Miller <silvanmiller@gmail.com>
2022-02-05 17:50:13 -0300

    arrumação

--- aula13ex/modelo/script.js
+++ aula13ex/modelo/script.js
@@ -1,17 +1,17 @@
 function tabuada() {
-    var numero = document.getElementById('txtn');
-    var tabuad = document.getElementById('seltabu');
+    let num = document.getElementById('txtn');
+    let tab = document.getElementById('seltabu');
 
-    if (numero.value.length == 0) {
+    if (num.value.length == 0) {
         alert('Por favor, Digite um número!');
     } else {
-        var n = Number(numero.value);
-        var c = 1;
+        let n = num.value;
+        let c = 1;
 
         while (c <= 10) {
             let item = document.createElement('option');
             item.text = `${n} x ${c} = ${n*c}`;
-            tabuad.appendChild(item);
+            tab.appendChild(item);
             c++
         }
 
