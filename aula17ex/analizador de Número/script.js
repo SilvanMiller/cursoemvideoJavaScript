fbb96f5c2192125f143187cf079dbdcaf4cbc472
Silvan Miller <silvanmiller@gmail.com>
2022-02-06 14:58:54 -0300

    atualização

--- aula17ex/analizador de Número/script.js
+++ aula17ex/analizador de Número/script.js
@@ -26,8 +26,43 @@ function adicionar() {
         valores.push(Number(num.value));
         let item = document.createElement('option');
         item.text = `O valor ${num.value} foi adicionado.`;
-        lista.appendChild(item)
+        lista.appendChild(item);
+        res.innerHTML = ''
     } else {
         alert(`O Valor já foi adicionado ou é invalido.`);
     }
+
+    num.value = '';
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert(`Adicione os números para finalizar!`);
+    } else {
+        let tot = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0;
+        let media = 0;
+
+        for (let maior_menor in valores) {
+            soma += valores[maior_menor];
+            if (valores[maior_menor] > maior)
+                maior = valores[maior_menor];
+            if (valores[maior_menor] < menor)
+                menor = valores[maior_menor];
+        }
+        media = soma / tot;
+
+        res.innerHTML = '';
+        res.innerHTML += `<p>Ao todo, Temos ${tot} número cadastrado.</p>`;
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`;
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`;
+        res.innerHTML += `<p> soma de todos os números é ${soma}.</p>`;
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`;
+
+    }
+
+
 }
